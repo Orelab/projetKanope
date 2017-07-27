328d8ed0281bc1a6fa66e17b06fbffc1b24f88c7
GregoryCLAUDE <claudejurado@orange.fr>
2017-07-27 14:04:08 +0200

    possibilité d'afficher les points en fonction des adresses et affichage des addresse au passage de la souris. Tout est dynamique. enregistrement des coordonées gps (lat et long) dans la base de données.

--- test/index.php
+++ test/index.php
@@ -7,56 +7,55 @@ try {
   die("Erreur : ".$e -> getMessage());
 }
 
-$reponse = $bdd->query("SELECT * FROM `TABLE 1` WHERE `COL 8`!='' ");
-
+$reponse = $bdd->query("SELECT * FROM `TABLE 1` WHERE `rue`!='' ");
 while($donnees=$reponse->fetch()){
-  $rue = $donnees['COL 8'];
-  $code_postal = $donnees['COL 10'];
-  $Ville = $donnees['COL 12'];
-$listaddress=[Rue=>$rue, CodePostal=>$code_postal, Ville=>$Ville, Lat=>"",Lng=>""];
-//$listaddressjson =json_encode($listaddress);
+    $id[] = $donnees["ID"];
+    $rue[] = $donnees['rue'];
+    $code_postal[] = $donnees['code postal'];
+    $Ville[] = $donnees['ville'];
+    $lat[] = $donnees['lat'];
+    $long[] = $donnees['long'];
+
+}
+for ($i=0; $i <count($rue) ; $i++) {
+  $listaddress[]=[ID=>$id[$i], Rue=>$rue[$i], CodePostal=>$code_postal[$i], Ville=>$Ville[$i], Lat=>$lat[$i], Lng=>$long[$i]];
+}
 
 
 
- // $listaddressutf8 =utf8_encode($listaddress);
 
 $geocoder = "http://maps.googleapis.com/maps/api/geocode/json?address=%s&sensor=false";
+  for ($i=0; $i<count($listaddress);++$i) {
+    if (strlen($listaddress[$i][Lat]) == "" && strlen($listaddress[$i][Lng]) == "") {
+       $adresse = $listaddress[$i][Rue].', '.$listaddress[$i][CodePostal].', '.$listaddress[$i][Ville];
+       // Requête envoyée à l'API Geocoding
+       $query = sprintf($geocoder, urlencode(utf8_encode($adresse)));
+       $result = json_decode(file_get_contents($query));
+       $json = $result->results[0];
+       $adress->Lat = (string) $json->geometry->location->lat;
+       $adress->Lng = (string) $json->geometry->location->lng;
 
-//$arrAddresses = adress::LoadAll(); // Notre collection d'objets Address
+       $lat = $adress->Lat;
+       $lng = $adress->Lng;
+       $listaddress[$i]["Lat"]=$lat;
+       $listaddress[$i]["Lng"]=$lng;
+       //$listaddress=[Lat=>$lat, Lng=>$lng];
+    }
+  }
+  $listaddressjson =json_encode($listaddress);
 
-foreach ($listaddress as $address) {
+  for ($i=0; $i<count($listaddress); $i++){
+    $lat = $listaddress[$i]["Lat"];
+    $lng = $listaddress[$i]["Lng"];
+    $id = $listaddress[$i]["ID"];
+    $bdd->exec("UPDATE `TABLE 1` SET latitude = $lat, longitude = $lng WHERE `ID` = $id");
 
+  }
 
-        if (strlen($listaddress[Lat]) == "" && strlen($listaddress[Lng]) == "") {
-
-            $adresse = $listaddress[Rue].', '.$listaddress[CodePostal].', '.$listaddress[Ville];
-
-
-            // Requête envoyée à l'API Geocoding
-            $query = sprintf($geocoder, urlencode(utf8_encode($adresse)));
-
-            $result = json_decode(file_get_contents($query));
-            $json = $result->results[0];
-
-            $adress->Lat = (string) $json->geometry->location->lat;
-            $adress->Lng = (string) $json->geometry->location->lng;
-
-            $lat = $adress->Lat;
-            $lng = $adress->Lng;
-            $listaddress["Lat"]=$lat;
-            $listaddress["Lng"]=$lng;
-            //$listaddress=[Lat=>$lat, Lng=>$lng];
-
-         }
-         var_dump($listadress);
-}
-
-}
+  //var_dump($listaddressjson);
 
 ?>
- <script type="text/javascript">
 
- </script>
 
 <html>
   <head>
@@ -97,34 +96,44 @@ foreach ($listaddress as $address) {
             maxZoom: 17,
             attribution: attrib
         });
+        osm.addTo(map);
+
     //     var circle = L.circle([43.631725, 0.588503], 500, {
     // color: 'red',
     // fillColor: '#f03',
     // fillOpacity: 0.5
     // }).addTo(map);
 
-        osm.addTo(map);
 
-        var markers = <?= $listaddressjson ?>;
+        var coord = <?= $listaddressjson ?>;
+        var markers = [];
+        var rue = [];
 
-        var marker = L.marker([43.6661798,0.600956181973427]).addTo(map);
-        var marker2 = L.marker([43.6689771,0.5977891]).addTo(map);
 
-        var popup = L.popup();
+        // for (var i = 0; i < coord.length; i++) {
+        //
+        //   markers.push(marker);
+        //
+        // }
 
+        for (var i = 0; i < coord.length; i++) {
+          // markers[i].addTo(map);
+
+
+
+        var marker = L.marker([coord[i].Lat, coord[i].Lng]).bindPopup(coord[i].Rue).addTo(map);
         marker.on('mouseover', function (e) {
-            popup.setLatLng(e.latlng)
-            .setContent("KANOPE Inno parc")
-            .openOn(map);
+          this.openPopup();
         });
-
         marker.on('mouseout', function (e) {
-            popup.closePopup();
+          this.closePopup();
         });
+      }
 
-    };
 
-    var listadressjson =
+     };
+
+
 
     document.addEventListener('DOMContentLoaded',
     InitialiserCarte());
