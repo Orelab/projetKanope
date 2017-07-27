f84a19d7a9b83b0dce05f382c30c41657f80c1cb
GregoryCLAUDE <claudejurado@orange.fr>
2017-07-27 14:51:03 +0200

    modification des noms de fichiers pour plus de clarté

--- test/adressToCoord.php
+++
@@ -1,9 +0,0 @@
-<?php
-try {
-  $bdd = new PDO("mysql:host=localhost; dbname=TABLE1; charset=utf8", "root", "root");
-} catch (Exception $e) {
-  die("Erreur : ".$e -> getMessage());
-}
-
-echo $bdd
- ?>
